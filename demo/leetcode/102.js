5ae6e9d774d3e7d465e950870e776d3cbb1f59d9
codytang <codytang@tencent.com>
2020-08-09 21:22:45 +0800

    docs: update leetcode

--- demo/leetcode/102.js
+++ demo/leetcode/102.js
@@ -11,16 +11,23 @@
  */
 var levelOrder = function (root) {
   if (!root) return [];
+  let queue = [root];
   let result = [];
-  dfs(root, 0, result);
+
+  while (queue.length > 0) {
+    const res = [];
+    let len = queue.length;
+
+    while (len) {
+      const node = queue.shift();
+      res.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+      len--;
+    }
+
+    result.push(res);
+  }
+
   return result;
 };
-
-function dfs(root, level, result) {
-  if (root) {
-    if (!result[level]) result[level] = [];
-    result[level].push(root.val);
-    dfs(root.left, level + 1, result);
-    dfs(root.right, level + 1, result);
-  }
-}
