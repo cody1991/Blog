ad9a2b6b8530941191f805b6ba7e0cf977037557
codytang <codytang@tencent.com>
2020-10-01 17:32:00 +0800

    refactor: restart ts

--- demo/typescript-demo/3-union.js
+++
@@ -1,7 +0,0 @@
-var myFavoriteNumber;
-myFavoriteNumber = 'seven';
-myFavoriteNumber = 7;
-function getLength(something) {
-    // length 不是 string 和 number 的共有属性，所以会报错
-    return something.length;
-}
--- demo/typescript-demo/3-union.ts
+++
@@ -1,8 +0,0 @@
-let myFavoriteNumber: string | number;
-myFavoriteNumber = 'seven';
-myFavoriteNumber = 7;
-
-function getLength(something: string | number): number {
-  // length 不是 string 和 number 的共有属性，所以会报错
-  return something.length;
-}
