5eca2334e53f60824f570b885855a21288403a95
codytang <codytang@tencent.com>
2021-03-19 13:51:00 +0800

    docs: 新增模板库博客

---
+++ demo/template/index.js
@@ -0,0 +1,43 @@
+const template = require('./template');
+
+console.log(
+  template(
+    `<span data-message='<%= 'hello' + 'world' %>'>
+      <%= name ? name: 1+1+1 %>
+      <% print('hello msg'); %>
+     </span>`,
+    {
+      name: 'codytang',
+    }
+  )
+);
+
+const myTemplate = template(
+  `<span data-message='<%= 'hello' + 'world' %>'>
+    <%= name ? name: 1+1+1 %>
+    <% print('hello msg'); %>
+   </span>`
+);
+
+console.log(myTemplate({ name: 'hello 2' }));
+
+function test(obj) {
+  var p = [],
+    print = function () {
+      p.push.apply(p, arguments);
+    };
+  with (obj) {
+    p.push(
+      "<span data-message='",
+      'hello' + 'world',
+      "'>       ",
+      name ? name : 1 + 1 + 1,
+      '       '
+    );
+    print('hello msg');
+    p.push('      </span>');
+  }
+  return p.join('');
+}
+
+console.log(test({ name: 'hello11' }));
