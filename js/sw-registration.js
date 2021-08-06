9c8321c3e9d96ee10140fe39a7e79b34c3e46e37
codytang <codytang@tencent.com>
2021-08-06 11:47:32 +0800

    🎉🎉 又发布新内容啦 🎉🎉

--- js/sw-registration.js
+++ js/sw-registration.js
@@ -31,11 +31,13 @@ function handleRegistration(registration) {
   };
 }
 
+var baseUrl = window.location.href.split('?')[0];
+
 if (navigator.serviceWorker) {
   // For security reasons, a service worker can only control the pages
   // that are in the same directory level or below it. That's why we put sw.js at ROOT level.
   navigator.serviceWorker
-    .register('sw.js')
+    .register('/Blog/sw.js')
     .then((registration) => handleRegistration(registration))
     .catch((error) => {
       console.log('ServiceWorker registration failed: ', error);
